ce1e18dd6ca688c58e5355a592dc15c903aa42ba
nagendra8357 <nagendra@cortiqahealth.com>
2025-05-28 17:17:56 +0530

    updated correct path bulidspec yaml file

--- src/App.js
+++ src/App.js
@@ -16,6 +16,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Simple React App</h1>
         </header>
+
+        <body className="App-body">
+          <p className="App-intro">
+            This is a simple React app to demonstrate routing and component rendering.
+          </p>
+        </body>
+       
           <Switch>
                 <Route exact path= "/" render={() => (
                   <Redirect to="/customerlist"/>
